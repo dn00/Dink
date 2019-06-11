20ba2a599805485a1d8347d50de8f4378928447f
dn00 <dtnguy90@gmail.com>
2019-06-11 03:56:31 -0700

    added ActionTypes and ActionHandler

---
+++ Dink/Actions/ActionHandler.cs
@@ -0,0 +1,26 @@
+﻿using Dink.Actions.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dink.Actions
+{
+    public class ActionHandler
+    {
+        private static ushort resultCode;
+        public static ushort DoAction(String noxName, ActionType actionType)
+        {
+            switch(actionType)
+            {
+                case ActionType.Respawn:
+                    {
+                        // call action respawn
+                        resultCode = 0;
+                        break;
+                    }
+            }
+
+            return resultCode;
+        }
+    }
+}
---
+++ Dink/Actions/Enum/ActionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dink.Actions.Enum
+{
+    public enum ActionType
+    {
+        Respawn = 0,
+
+    }
+}
